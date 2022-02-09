8bca4fec30b863431244672267c5c14fa3c59c81
Tonttu81 <joni.pylkkonen@gmail.com>
2022-02-09 11:48:35 +0200

    Lisäsin "post processing" packagen ja tein omaan sceneen pienen ensimmäisen proton pelistä.

--- Assets/Joni/TreeScript.cs
+++ Assets/Joni/TreeScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class TreeScript : MonoBehaviour
 {
-    [SerializeField]float loopTime = 1;
-    [SerializeField]float timer;
+    [SerializeField] float loopTime = 1;
+    [SerializeField] float timer;
 
-    [SerializeField]int treesInRadius;
+    [SerializeField] int treesInRadius;
 
     [SerializeField] LayerMask treeMask;
     public GameObject treePrefab;
@@ -15,7 +15,7 @@ public class TreeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,8 +28,8 @@ public class TreeScript : MonoBehaviour
             {
                 timer = loopTime;
                 float rng = Random.Range(1, 21);
-                
-                if (rng == 1)
+
+                if (rng >= 1)
                 {
                     SpawnTree();
                 }
@@ -44,14 +44,27 @@ public class TreeScript : MonoBehaviour
     void SpawnTree()
     {
         float x = Random.Range(-2f, 3f);
-        float z = Random.Range(-2f, 3f);
-        Vector3 treePos = new Vector3(transform.position.x - x, transform.position.y, transform.position.z - z);
 
-        Instantiate(treePrefab, treePos, Quaternion.identity);
+
+        float z = Random.Range(-2f, 3f);
+
+
+        Vector3 rayPos = new Vector3(transform.position.x - x, 10f, transform.position.z - z);
+
+        RaycastHit treePos;
+        Physics.Raycast(rayPos, Vector3.down, out treePos);
+
+        Collider[] overlap = Physics.OverlapSphere(treePos.point, 0.5f, treeMask);
+        if (overlap.Length == 0)
+        {
+            Instantiate(treePrefab, treePos.point, Quaternion.identity);
+        }
     }
 
     public void CutTree()
     {
         Destroy(gameObject);
     }
+
+
 }
