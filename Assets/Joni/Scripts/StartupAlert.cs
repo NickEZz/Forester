f0159522e4caaa2fe73233b630dc1f3875950d95
Tonttu81 <joni.pylkkonen@gmail.com>
2022-04-04 23:43:43 +0300

    Tehty ikkuna joka aukeaa kun pelaaja on saanut offline income

---
+++ Assets/Joni/Scripts/StartupAlert.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartupAlert : MonoBehaviour
+{
+    [SerializeField] GameObject alertWindow;
+    [SerializeField] TextMeshProUGUI alertText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShowAlert(float totalWood)
+    {
+
+    }
+}
